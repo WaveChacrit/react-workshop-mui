5c120f90b18f1912ba91bd3ac116876c9e1e9b17
WaveChacrit <wavewych@gmail.com>
2023-11-09 01:11:57 +0700

    :bento: Update image

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,18 @@ function App() {
 
   return (
     <Box sx={{ display: "flex", flexWrap: "wrap", gap: 4, margin: '60px' }}>
-      {/* <CardPluto />
+      <CardPluto />
       <CardPlanTrip />
       <CardBooking />
       <CardPluto2 />
       <CardPlanTrip />
       <CardPlanTrip2 />
       <CardBlog />
-      <CardTravelNorway /> */}
+      <CardTravelNorway />
       <CardListProperty name={"Chacrit"} profile={"https://images.unsplash.com/photo-1520975661595-6453be3f7070?auto=format&fit=crop&q=80&w=687&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/> 
       <CardListProperty name={"Pornthawee"} profile={"https://images.unsplash.com/photo-1497316730643-415fac54a2af?auto=format&fit=crop&q=80&w=1064&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
-      {/* <CardPropertyLatest/>
-      <CardListActivity/> */}
+      <CardPropertyLatest/>
+      <CardListActivity/>
       <CardTeamReview/>
       <CardPostHiring/>
     </Box>
